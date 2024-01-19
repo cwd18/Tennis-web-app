71aabc5eb47d6d50a881eb094d2adcc9c15c6be9
Charles Davies <charles.davies18@gmail.com>
2024-01-19 20:25:06 +0000

    Remove unneeded files

--- Deploy/RestartApache.sh
+++
@@ -1,5 +0,0 @@
-# Restart Apache
-apachectl stop
-echo "Apache stopped"
-apachectl start
-echo "Apache started"
--- Deploy/test.sh
+++
@@ -1,6 +0,0 @@
-sftp Charles@192.168.68.2 <<EOT
-lcd "/Users/charlesdavies/documents/tennis web app/public"
-cd /web/public
-put test.php index.php
-exit
-EOT
