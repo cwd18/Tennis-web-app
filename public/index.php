a50df088e7508b41eabf8f6ae1e0856fcc7ab9f8
Charles Davies <charles.davies18@gmail.com>
2024-02-18 20:01:15 +0000

    Forgot to save index.php

--- public/index.php
+++ public/index.php
@@ -89,7 +89,6 @@ $app->get('/fixtureresetplaying', \TennisApp\Action\FixtureResetPlaying::class);
 
 // Create participant routes
 $app->get('/participantPage', \TennisApp\Action\ParticipantPage::class);
-$app->get('/participantInvite', \TennisApp\Action\ParticipantInvite::class);
 $app->get('/participant', \TennisApp\Action\ParticipantView::class);
 $app->get('/participantWantsToPlay', \TennisApp\Action\ParticipantWantsToPlay::class);
 $app->get('/participantBook', \TennisApp\Action\ParticipantBook::class);
