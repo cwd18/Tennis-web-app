fc9cb1e072a5d84de606ab02d4cbc57e67220092
Charles Davies <charles.davies18@gmail.com>
2025-04-29 21:44:45 +0100

    Session lasts 4 weeks rather than 7 days

--- src/classes/SessionHandler.php
+++ src/classes/SessionHandler.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TennisApp;
@@ -12,56 +13,56 @@ class SessionHandler implements \SessionHandlerInterface
         $this->pdo = $pdo;
     }
 
-    public function open($savePath, $sessionName) : bool
+    public function open($savePath, $sessionName): bool
     {
         return true;
     }
 
-    public function close() : bool
+    public function close(): bool
     {
         return true;
     }
 
-    public function read($id) : string
+    public function read($id): string
     {
         $todayDate = date('Y-m-d');
         $sql = "SELECT SessionData FROM SessionData WHERE Sessionid = :id 
         AND SessionExpires > :today;";
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindParam('id', $id, \PDO::PARAM_INT);
-        $stmt->bindParam('today', $todayDate, \PDO::PARAM_STR); 
+        $stmt->bindParam('today', $todayDate, \PDO::PARAM_STR);
         $stmt->execute();
         $result = $stmt->fetchColumn();
         return $result == false ? "" : $result;
     }
 
-    public function write($id, $sessionData) : bool
+    public function write($id, $sessionData): bool
     {
         $todayDate = date('Y-m-d');
         $sql = "REPLACE INTO SessionData 
-        SET Sessionid = :id, SessionExpires = DATE_ADD(:today, INTERVAL 7 DAY), 
+        SET Sessionid = :id, SessionExpires = DATE_ADD(:today, INTERVAL 4 WEEK), 
         SessionData = :SessionData;";
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindParam('id', $id, \PDO::PARAM_INT);
-        $stmt->bindParam('today', $todayDate, \PDO::PARAM_STR); 
-        $stmt->bindParam('SessionData', $sessionData, \PDO::PARAM_STR); 
+        $stmt->bindParam('today', $todayDate, \PDO::PARAM_STR);
+        $stmt->bindParam('SessionData', $sessionData, \PDO::PARAM_STR);
         $stmt->execute();
         return true;
     }
 
-    public function destroy($id) : bool
+    public function destroy($id): bool
     {
         $sql = "DELETE FROM SessionData WHERE Sessionid = :id";
-        $this->pdo->runSQL($sql,['id' => $id]); 
+        $this->pdo->runSQL($sql, ['id' => $id]);
         return true;
     }
 
-    public function gc($maxlifetime) : int|false
+    public function gc($maxlifetime): int|false
     {
         // Cleanup old sessions, returning the number of deleted sessions on success, or false on failure
         $todayDate = date('Y-m-d');
         $stmt = $this->pdo->prepare("DELETE FROM SessionData WHERE SessionExpires < :today;");
-        $stmt->bindParam('today', $todayDate, \PDO::PARAM_STR); 
+        $stmt->bindParam('today', $todayDate, \PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->rowCount();
     }
